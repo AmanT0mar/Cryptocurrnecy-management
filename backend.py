4087071efdf7025f4cea60ffd69cd1c03a52cea1
AmanT0mar <75624000+AmanTomar69@users.noreply.github.com>
2022-11-29 16:56:32 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -8,6 +8,7 @@ import time
 mydb = mysql.connector.connect(host = "localhost", user="Aman", database = "mydb")
 mycursor = mydb.cursor()
 
+#FOR SIGNUP
 def selectfromusers(username):
     sql = "SELECT * FROM USER_INFO WHERE USERNAME = %s "
     val = (username,)
@@ -15,13 +16,13 @@ def selectfromusers(username):
     userdata = mycursor.fetchall()
     for i in userdata:
         return i
-        
+#FOR SIGNUP    
 def insertintousers(fullname,username,phno,passwd):
     sql = "INSERT INTO USER_INFO VALUES(%s,%s,%s,%s)"
     val = (fullname,username,phno,passwd,)
     mycursor.execute(sql,val)
     mydb.commit()
-    
+#FOR LOGIN    
 def logindatabase(username,passwd):
     sql = "SELECT * FROM USER_INFO WHERE USERNAME = %s"
     val = (username,)
@@ -30,27 +31,26 @@ def logindatabase(username,passwd):
     for i in logindata:
         if passwd == i[3]:
             return True
-        
+#FOR HISTORICAL DATA    
 def get_his(curname,interval):#interval = 1d,1week,1month,1year 
     #if want more data points use start and end in .get()
     if interval == '1d':
         data = requests.get("http://api.coincap.io/v2/assets/"+f'{curname}'+"/history?interval=m1")
         data = data.json()
         for i in data['data']:
-            print(i['priceUsd'])
+            print(i['priceUsd'],i['time'])
     elif interval == '1w':
         data = requests.get("http://api.coincap.io/v2/assets/"+f'{curname}'+"/history?interval=m15")
         data = data.json()
         for i in data['data']:
-            print(i['priceUsd'])
+            print(i['priceUsd'],i['time'])
     elif interval == '1m':
         data = requests.get("http://api.coincap.io/v2/assets/"+f'{curname}'+"/history?interval=h1")
         data = data.json()
         for i in data['data']:
-            print(i['priceUsd'])
+            print(i['priceUsd'],i['time'])
     elif interval == '1y':
         data = requests.get("http://api.coincap.io/v2/assets/"+f'{curname}'+"/history?interval=h12")
         data = data.json()
         for i in data['data']:
-            print(i['priceUsd'])
-        
+            print(i['priceUsd'],i['time'])
