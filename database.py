bc73dd567bb8277c72fada2c55576ef2340dbbb3
AmanT0mar <75624000+AmanTomar69@users.noreply.github.com>
2022-12-01 12:55:38 +0530

    Update database.py

--- database.py
+++ database.py
@@ -4,53 +4,53 @@ mydb = mysql.connector.connect(host = "localhost", user="Aman", database = "mydb
 mycursor = mydb.cursor()
 
 mycursor.execute("""CREATE TABLE IF NOT EXISTS USER_INFO
-                (FULLNAME VARCHAR(30),
-                USERNAME VARCHAR(30),
-                PHONENO BIGINT,
-                PASSWORD VARCHAR(15),
+                (FULLNAME VARCHAR(30) NOT NULL,
+                USERNAME VARCHAR(30) NOT NULL,
+                PHONENO BIGINT(10) NOT NULL,
+                PASSWORD VARCHAR(15) NOT NULL,
                 CONSTRAINT PK_U PRIMARY KEY(USERNAME)""")
 
 mycursor.execute("""CREATE TABLE IF NOT EXISTS SELL_OUT
-                (USERNAME VARCHAR(30),
-                SYMBOL VARCHAR(7),
-                CURNAME VARCHAR(15),
-                PRICE DECIMAL(22,16),
-                QUANTITY DECIMAL(8,3),
-                TOTAL_RETURNS DECIMAL(15,8),
-                SELL_TIME TIMESTAMP,
+                (USERNAME VARCHAR(30) NOT NULL,
+                SYMBOL VARCHAR(7) NOT NULL,
+                CURNAME VARCHAR(15) NOT NULL,
+                PRICE DECIMAL(22,16) NOT NULL,
+                QUANTITY DECIMAL(8,3) NOT NULL,
+                TOTAL_RETURNS DECIMAL(15,8) NOT NULL,
+                SELL_TIME TIMESTAMP NOT NULL,
                 CONSTRAINT PK_S PRIMARY KEY(USERNAME,CURNAME,QUANTITY,SELL_TIME),
                 CONSTRAINT FK_S FOREIGN KEY(USERNAME) REFERENCES USER_INFO(USERNAME) ON DELETE CASCADE)""")
 
 mycursor.execute("""CREATE TABLE IF NOT EXISTS BOUGHT
-                 (USERNAME VARCHAR(30),
-                 SYMBOL VARCHAR(7),
-                 CURNAME VARCHAR(15),
-                 PRICE DECIMAL(22,16),
-                 QUANTITY DECIMAL(8,3),
-                 TOTAL_PRICE DECIMAL(15,8),
-                 BUY_TIME TIMESTAMP,
+                 (USERNAME VARCHAR(30) NOT NULL,
+                 SYMBOL VARCHAR(7) NOT NULL,
+                 CURNAME VARCHAR(15) NOT NULL,
+                 PRICE DECIMAL(22,16) NOT NULL,
+                 QUANTITY DECIMAL(8,3) NOT NULL,
+                 TOTAL_PRICE DECIMAL(15,8) NOT NULL,
+                 BUY_TIME TIMESTAMP NOT NULL,
                  CONSTRAINT PK_B PRIMARY KEY(USERNAME,CURNAME,BUY_TIME),
                  CONSTRAINT FK_B FOREIGN KEY(USERNAME) REFERENCES USER_INFO (USERNAME) ON DELETE CASCADE)""")
 
 mycursor.execute("""CREATE TABLE IF NOT EXISTS WATCHLIST
-                 (USERNAME VARCHAR(30),
-                 SYMBOL VARCHAR(7),
-                 CURNAME VARCHAR(15),
-                 PRICE DECIMAL(22,16),
-                 SUPPLY DECIMAL(26,16),
-                 MARKETCAP DECIMAL(35,16),
-                 VOLUME DECIMAL(35,16),
+                 (USERNAME VARCHAR(30) NOT NULL,
+                 SYMBOL VARCHAR(7) NOT NULL,
+                 CURNAME VARCHAR(15) NOT NULL,
+                 PRICE DECIMAL(22,16) NOT NULL,
+                 SUPPLY DECIMAL(26,16) NOT NULL,
+                 MARKETCAP DECIMAL(35,16) NOT NULL,
+                 VOLUME DECIMAL(35,16) NOT NULL,
                  CONSTRAINT PK_W PRIMARY KEY(USERNAME,CURNAME),
                  CONSTRAINT FK_W FOREIGN KEY(USERNAME) REFERENCES USER_INFO (USERNAME) ON DELETE CASCADE)""")
 mycursor.execute(""" CREATE TABLE IF NOT EXISTS HOLDING
-                 (USERNAME VARCHAR(30),
-                 SYMBOL VARCHAR(7),
-                 CURNAME VARCHAR(15),
-                 CUR_PRICE DECIMAL(22,16),
-                 QUANTITY DECIMAL(8,3),
-                 INVESTED DECIMAL(30,16),
-                 PER_COIN DECIMAL(22,16),
-                 RETURNS DECIMAL(30,16),
+                 (USERNAME VARCHAR(30) NOT NULL,
+                 SYMBOL VARCHAR(7) NOT NULL,
+                 CURNAME VARCHAR(15) NOT NULL,
+                 CUR_PRICE DECIMAL(22,16) NOT NULL,
+                 QUANTITY DECIMAL(8,3) NOT NULL,
+                 INVESTED DECIMAL(30,16) NOT NULL,
+                 PER_COIN DECIMAL(22,16) NOT NULL,
+                 RETURNS DECIMAL(30,16) ,
                  CONSTRAINT PK_H PRIMARY KEY(USERNAME,CURNAME,QUANTITY),
                  CONSTRAINT FK_H FOREIGN KEY(USERNAME) REFERENCES USER_INFO(USERNAME) ON DELETE CASCADE)""")
 
