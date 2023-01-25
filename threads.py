5f01d63a11ec69f748166803cb3bccd77dcb2b08
AmanT0mar <75624000+AmanTomar69@users.noreply.github.com>
2023-01-25 18:06:27 +0530

    Update threads.py

--- threads.py
+++ threads.py
@@ -8,44 +8,46 @@ import time
 from decimal import Decimal
 from datetime import datetime
 
-import mysql.connector
-
-mydb = mysql.connector.connect(host = "localhost", user="Aman", database = "myproj")
+mydb = mysql.connector.connect(host = "localhost", user="Aman", database = "MYDB")
 mycursor = mydb.cursor()
 
 def up_wl(username):
-    mycursor.execute("SELECT * FROM WATCHLIST")
-    w_data = mycursor.fetchall()
-    for i in range(len(w_data)):
-        curname = w_data[i][2]
-        data = requests.get("http://api.coincap.io/v2/assets/"+f'{curname.lower()}')
-        data = data.json()
-        a = data['data']
-        mycursor.execute(f"UPDATE WATCHLIST SET PRICE = {Decimal(a['priceUsd'])}, SUPPLY = {Decimal(a['supply'])}, MARKETCAP = {Decimal(a['marketCapUsd'])}, VOLUME = {Decimal(a['volumeUsd24Hr'])} WHERE CURNAME = '{curname}' AND USERNAME = {username}")
-        mydb.commit() 
+    mycursor.execute(f"SELECT CURNAME FROM WATCHLIST WHERE USERNAME = '{username}'")
+    c_names = mycursor.fetchall()
+    for i in range(len(c_names)):
+        mycursor.execute(f"SELECT CID FROM COINS WHERE CNAME = '{c_names[i][0]}'")
+        c_id = mycursor.fetchall()
+        for j in c_id:
+            data = requests.get("http://api.coincap.io/v2/assets/"+f'{j[0]}')
+            data = data.json()
+            a = data['data']
+            mycursor.execute(f"UPDATE WATCHLIST SET PRICE = {Decimal(a['priceUsd'])}, SUPPLY = {Decimal(a['supply'])}, MARKETCAP = {Decimal(a['marketCapUsd'])}, VOLUME = {Decimal(a['volumeUsd24Hr'])} WHERE CURNAME = '{c_names[i][0]}' AND USERNAME = '{username}'")
+            mydb.commit()
+            print("updated watchlist")
 def up_hd(username):        
-    mycursor.execute("SELECT * FROM HOLDING")
+    mycursor.execute(f"SELECT * FROM HOLDING WHERE USERNAME = '{username}'")
     h_data = mycursor.fetchall()
     for i in range(len(h_data)):
-        curname = h_data[i][2]
-        inv = h_data[i][5]
-        q = h_data[i][4]
-        data = requests.get("http://api.coincap.io/v2/assets/"+f'{curname.lower()}')
-        data = data.json()
-        a = data['data']
-        returns = inv - Decimal(a['priceUsd'])*q
-        mycursor.execute(f"UPDATE HOLDING SET CUR_PRICE = {Decimal(a['priceUsd'])}, RETURNS ={returns} WHERE CURNAME = '{curname}' AND USERNAME = {username}")
-        mydb.commit() 
+        mycursor.execute(f"SELECT CID FROM COINS WHERE CNAME = '{h_data[i][2]}'")
+        c_id = mycursor.fetchall()
+        for j in c_id:
+            inv = h_data[i][5]
+            q = h_data[i][4]
+            data = requests.get("http://api.coincap.io/v2/assets/"+f'{j[0]}')
+            data = data.json()
+            a = data['data']
+            returns = Decimal(a['priceUsd'])*q - inv
+            mycursor.execute(f"UPDATE HOLDING SET CUR_PRICE = {Decimal(a['priceUsd'])}, RETURNS ={returns} WHERE USERNAME = '{username}' AND CURNAME = '{h_data[i][2]}'")
+            mydb.commit()
+            print("update holding")
 def thr():
-    t1 = threading.Thread(target=up_wl('superman'))
-    t2 = threading.Thread(target=up_hd('superman'))
+    t1 = threading.Thread(target=up_wl('aman1234'))
+    t2 = threading.Thread(target=up_hd('aman1234'))
     t1.start()
     t2.start()
     t1.join()
     t2.join()
-
-    
 while True:
     thr()
-    time.sleep(30)
+    time.sleep(15)
     
